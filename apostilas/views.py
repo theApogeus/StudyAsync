43b3e94fef488365a10248ab1ddda6e9313933d3
theApogeus <133257479+theApogeus@users.noreply.github.com>
2024-01-22 21:32:59 -0300

    Update views.py

--- apostilas/views.py
+++ apostilas/views.py
@@ -5,40 +5,46 @@ from django.contrib.messages import constants
 from django.contrib import messages
 
 def adicionar_apostilas(request):
-    if request.method == 'GET':
-        apostilas = Apostila.objects.filter(user=request.user)
-        # TODO: Criar as tags
-        views_totais = ViewApostila.objects.filter(apostila__user = request.user).count()
-        return render(request, 'adicionar_apostilas.html', { 'apostilas': apostilas,
-                                                            'views_totais': views_totais })
-    elif request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        arquivo = request.FILES['arquivo']
+  if not request.user.is_authenticated:
+    return redirect('/usuarios/login')
+  
+  if request.method == 'GET':
+    apostilas = Apostila.objects.filter(user=request.user)
+    # TODO: Criar as tags
+    views_totais = ViewApostila.objects.filter(apostila__user = request.user).count()
+    return render(request, 'adicionar_apostilas.html', { 'apostilas': apostilas,
+                                                        'views_totais': views_totais })
+  elif request.method == 'POST':
+    titulo = request.POST.get('titulo')
+    arquivo = request.FILES['arquivo']
 
-        apostila = Apostila(
-            user = request.user,
-            titulo = titulo,
-            arquivo = arquivo
-        )
+    apostila = Apostila(
+        user = request.user,
+        titulo = titulo,
+        arquivo = arquivo
+    )
 
-        apostila.save()
-        messages.add_message(request, constants.SUCCESS, 'Salvo com sucesso!')
-        return redirect('/apostilas/adicionar_apostilas')
+    apostila.save()
+    messages.add_message(request, constants.SUCCESS, 'Salvo com sucesso!')
+    return redirect('/apostilas/adicionar_apostilas')
     
 
 def apostila(request, id):
-    apostila = Apostila.objects.get(id = id)
+  if not request.user.is_authenticated:
+    return redirect('/usuarios/login')
+  
+  apostila = Apostila.objects.get(id = id)
 
-    views_unicas = ViewApostila.objects.filter(apostila=apostila).values('ip').distinct().count()
-    views_totais = ViewApostila.objects.filter(apostila=apostila).count()
+  views_unicas = ViewApostila.objects.filter(apostila=apostila).values('ip').distinct().count()
+  views_totais = ViewApostila.objects.filter(apostila=apostila).count()
 
-    view = ViewApostila(
-        ip = request.META['REMOTE_ADDR'],
-        apostila = apostila
-    )
-    view.save()
+  view = ViewApostila(
+      ip = request.META['REMOTE_ADDR'],
+      apostila = apostila
+  )
+  view.save()
 
-    return render(request, 'apostila.html', {'apostila': apostila,
-                                             'views_unicas': views_unicas,
-                                             'views_totais': views_totais})
+  return render(request, 'apostila.html', {'apostila': apostila,
+                                              'views_unicas': views_unicas,
+                                              'views_totais': views_totais})
 
